a662f90512c6ecce93b2d85244d69ddccc81e320
Ihsan Satriawan <ihsan.satriawan.20@gmail.com>
2022-11-29 05:10:40 +0700

    Initial Commit

---
+++ GoTravel/GoTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoTravelApp.swift
+//  GoTravel
+//
+//  Created by ihsan.satriawan on 29/11/22.
+//
+
+import SwiftUI
+
+@main
+struct GoTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
